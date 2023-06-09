2d4a2e24c6e300864266ec5db7791b222f7f2490
bammytech1 <66552185+bammytech1@users.noreply.github.com>
2023-06-09 18:06:04 +0100

    build2

--- elearning-app/src/data.jsx
+++ elearning-app/src/data.jsx
@@ -1,27 +1,27 @@
-// import  './assets/tech-people.png';
+import workPic from "../src/assets/Bitmap.svg";
 
 const portfolioData = [
     {
         id: 1,
-        coverImage: './Bitmap.svg',
+        coverImage: workPic,
         category: 'web-design',
         title: 'Bamstore'
     },
     {
         id: 2,
-        coverImage: './Bitmap.svg',
+        coverImage: workPic,
         category: 'web-design',
         title: 'Bamstore'
     },
     {
         id: 3,
-        coverImage: './Bitmap.svg',
+        coverImage: workPic,
         category: 'web-design',
         title: 'Bamstore'
     },
     {
         id: 4,
-        coverImage: './Bitmap.svg',
+        coverImage: workPic,
         category: 'web-design',
         title: 'Bamstore'
     },
--- elearning-app/src/teamdata.jsx
+++ elearning-app/src/teamdata.jsx
@@ -1,24 +1,24 @@
-// import  '../src/assets/ceo-avatar.svg'
+import teamPic from  '../src/assets/ceo-avatar.svg'
 
     
 const teamData = [
     {
         id: 1,
-        avatar: '../src/assets/ceo-profile.jpg',
+        avatar: teamPic,
         name: 'Tolu Owolabi',
         position: 'Dev-up Engineer',
         experience: ['Back-End', 'Azure', 'Python', 'Data engineer'],
     },
     {
         id: 2,
-        avatar: '../src/assets/gbenga.png',
+        avatar: teamPic,
         name: 'Gbenga Bayode',
         position: 'Product Manager',
         experience: ['Designer', 'UX/UI', 'User experience', 'Front-End'],
     },
     {
         id: 3,
-        avatar: './ceo-profile.jpg',
+        avatar: teamPic,
         name: 'Tolu Owolabi',
         position: 'Product Manager',
         experience: ['Designer', 'UX/UI', 'User experience', 'Front-End'],
